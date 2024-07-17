08017cb075f417b45f73b62aece20d4784c4430a
PhanDuy <duyphan1305@gmail.com>
2024-07-17 11:46:21 +0700

    first commit

---
+++ src/main/java/com/example/ITStudentManagementSystem/ItStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ITStudentManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItStudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItStudentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ITStudentManagementSystem/ItStudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ITStudentManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItStudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
